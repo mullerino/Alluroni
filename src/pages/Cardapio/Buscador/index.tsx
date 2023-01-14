bd3fb16a9d7c600be2731629b8128a3124cd8bde
Leandro <leandromllr9@gmail.com>
2023-01-14 12:39:48 -0300

    feat: adicionando buscador responsável por filtrar os pratos pelo texto inserido

---
+++ src/pages/Cardapio/Buscador/index.tsx
@@ -0,0 +1,18 @@
+import styles from './buscador.module.scss'
+import { CgSearch} from 'react-icons/cg'
+
+interface buscadorProps{
+    busca: string;
+    setBusca: React.Dispatch<React.SetStateAction<string>>
+}
+
+const Buscador = ({ busca, setBusca}: buscadorProps)=>{
+    return (
+        <div className={styles.buscador}>
+            <input type='text' value={busca} onChange={e=>setBusca(e.target.value)}></input>
+            <CgSearch size={20} color='#4C4D5E'/>
+        </div>
+    )
+}
+
+export default Buscador
