d47fba29cfa7b68dc72cd3a0e99f0a3ba9ed5bcc
Leandro <leandromllr9@gmail.com>
2023-01-14 12:41:55 -0300

    feat: criando state para o componente Filtros

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,7 +1,14 @@
 import styles from './Cardapio.module.scss'
 import logo from '../../assets/logo.svg'
+import Buscador from './Buscador'
+import { useState } from 'react'
+import Filtros from './Filtros'
+
 
 const Cardapio = ()=>{
+    const [busca,setBusca] = useState<string>('')
+    const [filtro,setFiltro] = useState<number | null>(null)
+
     return (
         <main>
             <nav className={styles.menu}>
@@ -12,6 +19,13 @@ const Cardapio = ()=>{
                     A casa do código na massa
                 </div>
             </header>
+            <section className={styles.cardapio}>
+                <h3 className={styles.cardapio_titulo}>Cardápio</h3>
+                <Buscador busca = {busca} setBusca = {setBusca}/>
+                <div className={styles.cardapio_filtros}>
+                    <Filtros filtro ={filtro} setFiltro={setFiltro}></Filtros>
+                </div>
+            </section>
         </main>
     )
 }
